59b83403ccac442fff6b98679d866604cc145a9b
anubhav8804602148@gmail.com <anubhav8804602148@gmail.com>
2024-02-19 23:06:01 +0530

    Aspect Logging activated

--- war-manager-common/src/main/java/com/war/manager/common/config/AspectLogging.java
+++ war-manager-common/src/main/java/com/war/manager/common/config/AspectLogging.java
@@ -3,6 +3,7 @@ package com.war.manager.common.config;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
@@ -10,21 +11,22 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@Aspect
 public class AspectLogging {
 	
-	@Before("@annotation(WarManagerLoggable)")
+	@Before("execution(* com.war..*(..))")
     public void logBeforeMethodInvocation(JoinPoint joinPoint) {
-        log.info("Before " + joinPoint.getSignature().toShortString() + " execution");
+        log.info("Log Before " + joinPoint.getSignature().toShortString() + " execution");
     }
 
-    @AfterThrowing(pointcut = "@annotation(WarManagerLoggable)", throwing = "exception")
+    @AfterThrowing(pointcut = "execution(* com.war..*(..))", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        log.error("Exception in " + joinPoint.getSignature().toShortString() + ": " + exception.getMessage());
+        log.error("Log Exception in " + joinPoint.getSignature().toShortString() + ": " + exception.getMessage());
     }
 	
 
-    @AfterReturning(pointcut = "@annotation(WarManagerLoggable)")
+    @AfterReturning(pointcut = "execution(* com.war..*(..))")
     public void logAfterSuccess(JoinPoint joinPoint) {
-        log.info("Success: " + joinPoint.getSignature().toShortString());
+        log.info("Log Success: " + joinPoint.getSignature().toShortString());
     }
 }
--- war-manager-common/src/main/java/com/war/manager/common/config/WarManagerLoggable.java
+++
@@ -1,5 +0,0 @@
-package com.war.manager.common.config;
-
-public @interface WarManagerLoggable {
-
-}
